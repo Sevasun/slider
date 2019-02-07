7c2125f7e5922d7ecd1b1cfbf6eddcfc460f1b10
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-02-07 23:34:22 +0200

    button events & autoplay added

--- js/main.js
+++ js/main.js
@@ -3,16 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	let slider = new Slider({
 		slider: '.slider',
 		slideset: '.slideset',
-		slide: '.slide',
-		prevBtn: '.btn-prev',
-		nextBtn: '.btn-next',
-		autoplay: true,
-		autoplayDelay: 5000,
-		animationSpeed: 1000,
-		pagination: '.pagination'
+		slide: '.slide'
 	});
 
-	slider.moveRight();
+	let carousel = new Slider({
+		slider: '.carousel',
+		autoplay: true
+	});
 });
 
 function Slider(options) {
@@ -28,19 +25,19 @@ function Slider(options) {
 		pagination: '.pagination'
 	};
 
-	let context = this;
-
 	let settings = merge(options, defaultOptions);
 
+	let context = this;
+
 	this.slider = document.querySelector(settings.slider);
-	this.slideset = context.slider.querySelector(settings.slideset);
-	this.slide = context.slideset.querySelectorAll(settings.slide);
+	this.slideset = this.slider.querySelector(settings.slideset);
+	this.slide = this.slideset.querySelectorAll(settings.slide);
 	this.autoplay = settings.autoplay;
 	this.autoplayDelay = settings.autoplayDelay;
 	this.autoplaySpeed = settings.autoplaySpeed;
-	this.prevBtn = context.slider.querySelector(settings.prevBtn);
-	this.nextBtn = context.slider.querySelector(settings.nextBtn);
-	this.pagination = context.slider.querySelector(settings.pagination);
+	this.prevBtn = this.slider.querySelector(settings.prevBtn);
+	this.nextBtn = this.slider.querySelector(settings.nextBtn);
+	this.pagination = this.slider.querySelector(settings.pagination);
 
 	let i = 0,
 		offset = 0,
@@ -53,24 +50,18 @@ function Slider(options) {
 		currentSlide.classList.remove('current');
 		currentSlide = context.slide[i];
 		currentSlide.classList.add('current');
-		context.slideset.style.transform = 'translateX(${offset});';
+		context.slideset.style.transform = `translateX(${offset}px)`;
 	};
 
 	this.moveRight = function() {
 		i++;
-		console.log(i);
 		if (i < context.slide.length) {
 			offset -= slideWidth;
 		} else {
 			i = 0;
 			offset = 0;
 		};
-		currentSlide.classList.remove('current');
-		currentSlide = context.slide[i];
-		currentSlide.classList.add('current');
-		context.slideset.style = "margin-left:" + offset;
-		console.log(context.slideset.style.marginLeft);
-		// context.slideset.style = "margin-left:10px"
+		move();
 	};
 
 	this.moveLeft = function() {
@@ -81,13 +72,23 @@ function Slider(options) {
 			i = context.slide.length - 1;
 			offset = -1 * slideWidth * (context.slide.length - 1);
 		};
-		move;
+		move();
 	};
+
+	if (this.autoplay) {
+		context.slideset.style = `transition-duration:${this.autoplaySpeed}ms`;
+		setInterval(context.moveRight, context.autoplayDelay);
+	};
+
+	this.nextBtn.addEventListener('click', function(e) {
+		e.preventDefault();
+		context.moveRight();
+	});
+	this.prevBtn.addEventListener('click', this.moveLeft);
 };
 
 function merge(obj1, obj2) {
 	let newObj = {};
-
 	for (let key in obj2) {
 		if (!obj1.hasOwnProperty(key)) {
 			obj1[key] = obj2[key];
