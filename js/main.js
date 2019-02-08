474903a983eeef9fcd1e10209af4f8962ba77dff
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-02-08 20:21:28 +0200

    pagination buttons create

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	let slider = new Slider({
 		slider: '.slider',
-		slideset: '.slideset',
-		slide: '.slide'
+		pagination: true
 	});
 
 	let carousel = new Slider({
@@ -22,7 +21,7 @@ function Slider(options) {
 		autoplay: false,
 		autoplayDelay: 5000,
 		animationSpeed: 1000,
-		pagination: '.pagination'
+		pagination: false
 	};
 
 	let settings = merge(options, defaultOptions);
@@ -37,7 +36,7 @@ function Slider(options) {
 	this.autoplaySpeed = settings.autoplaySpeed;
 	this.prevBtn = this.slider.querySelector(settings.prevBtn);
 	this.nextBtn = this.slider.querySelector(settings.nextBtn);
-	this.pagination = this.slider.querySelector(settings.pagination);
+	this.pagination = settings.pagination;
 
 	let i = 0,
 		offset = 0,
@@ -75,6 +74,25 @@ function Slider(options) {
 		move();
 	};
 
+	if (this.pagination) {
+		let pageList = createPagination(context.slider);
+
+		for (let j = 0; j < this.slide.length; j++) {
+			createPaginationButton(pageList, j);
+		};
+
+		let paginationItem = pageList.querySelectorAll('li');
+
+		console.log(paginationItem);
+
+		for (let j = 0; j < paginationItem.length; j++) {
+			let pageBtn = paginationItem[j].querySelector('.pagination-btn');
+			pageBtn.addEventListener('click', function() {
+				console.log(j);
+			});
+		};
+	};
+
 	if (this.autoplay) {
 		context.slideset.style = `transition-duration:${this.autoplaySpeed}ms`;
 		setInterval(context.moveRight, context.autoplayDelay);
@@ -84,7 +102,10 @@ function Slider(options) {
 		e.preventDefault();
 		context.moveRight();
 	});
-	this.prevBtn.addEventListener('click', this.moveLeft);
+	this.prevBtn.addEventListener('click', function(e) {
+		e.preventDefault();
+		context.moveLeft();
+	});
 };
 
 function merge(obj1, obj2) {
@@ -96,3 +117,22 @@ function merge(obj1, obj2) {
 	};
 	return obj1;
 };
+
+createPagination = function(holder) {
+	let pageBlock = document.createElement('div');
+	pageBlock.classList.add('pagination');
+	let pageList = document.createElement('ul');
+	pageList.classList.add('pagination-list');
+	pageBlock = holder.appendChild(pageBlock);
+	pageList = pageBlock.appendChild(pageList);
+	return pageList;
+};
+
+createPaginationButton = function(list, counter) {
+	let paginationLi = document.createElement('li');
+	paginationLi = list.appendChild(paginationLi);
+	let paginationButton = document.createElement('button');
+	paginationButton.classList.add('pagination-btn');
+	paginationButton.innerHTML = `<span class="number">${counter + 1}</span>`;
+	paginationButton = paginationLi.appendChild(paginationButton);
+}
