42fde9c188ffbc77dcfe9fa0815012057973df1b
Yevgen Sereda <yevgen.sereda@jelvix.com>
2019-02-12 00:15:09 +0200

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* global require __dirname module */
+let path = require('path');
+
+let conf = {
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, './js'),
+        filename: 'script.js',
+        publicPath: 'js/'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    }
+}
+
+module.exports = (env, options) => {
+    conf.devtool = options.mode === "production" ?
+                    "source-map" :
+                    "cheap-module-eval-source-map";
+    return conf;
+}
